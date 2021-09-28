b72dc5b655657ee86487fdc7f3edfed3eac6d8cc
tejnc <tejnarayan15.tnc@gmail.com>
2021-09-29 04:48:09 +0545

    A minimal api format

--- task1/main.py
+++ task1/main.py
@@ -1,13 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+class HelloWorld(Resource):
+    def get(self):
+        return{'hello':'world'}
 
-@app.route('/check/<string:email>')
-def check_info(email):
-    return "received"
+api.add_resource(HelloWorld,'/')
 
 if __name__ == "__main__":
     app.run(debug=True)
