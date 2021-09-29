004ecfd229ef22a7fea3edfdf2b80b90160b39e4
tejnc <tejnarayan15.tnc@gmail.com>
2021-09-30 03:05:36 +0545

    1st api

--- task1/main.py
+++ task1/main.py
@@ -1,14 +1,37 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from os import name
+from flask import Flask, json, request, jsonify
+import sqlite3
 
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return{'hello':'world'}
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('users.sqlite')
+    except sqlite3.error as e:
+        print(e)
+    return conn
 
-api.add_resource(HelloWorld,'/')
+#According to specification this only needs post method
+@app.route('/user',methods=['GET','POST'])
+def users():
+    conn = db_connection()
+    cursor = conn.cursor()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    if request.method == 'GET':
+        cursor=conn.execute("SELECT * FROM user")
+        #dict comprehension
+        users = [dict(email=row[0],name=row[1]) for row in cursor.fetchall()]
+
+        if users is not None:
+            return jsonify(users)
+
+
+    if request.method == 'POST':
+        new_email = request.form['email']
+        new_name = request.form['name']
+
+        sql="""INSERT INTO user(email,name) VALUES(?,?)"""
+        cursor = cursor.execute(sql,(new_email,new_name))
+        conn.commit()
+        return f"{new_email} has been created successfully."
