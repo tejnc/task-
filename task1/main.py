95f3c2858c633f8839924d96d42c583f4c81c30a
tejnc <tejnarayan15.tnc@gmail.com>
2021-09-30 09:53:14 +0545

    Finally 1st api completed

--- task1/main.py
+++ task1/main.py
@@ -30,8 +30,20 @@ def users():
     if request.method == 'POST':
         new_email = request.form['email']
         new_name = request.form['name']
+        reg_user = False
 
-        sql="""INSERT INTO user(email,name) VALUES(?,?)"""
-        cursor = cursor.execute(sql,(new_email,new_name))
-        conn.commit()
-        return f"{new_email} has been created successfully."
+        cursor = conn.execute("SELECT email from user")
+        for row in cursor:
+            if new_email == row[0]:
+                reg_user = True
+        # cursor.execute("SELECT * FROM user WHERE id=?",(new_email))
+        # rows = cursor.fetchall()
+        
+        
+        if reg_user == False:
+            sql="""INSERT INTO user(email,name) VALUES(?,?)"""
+            cursor = cursor.execute(sql,(new_email,new_name))
+            conn.commit()
+            return f"{new_email} has been created successfully."
+        else:
+            return 'You are already our member.'
